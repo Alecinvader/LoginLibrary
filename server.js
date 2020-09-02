0d9dd8ee5a3db36cf90363a63b4647019d5dcd38
alec-r-reynolds <alechasrito@gmail.com>
2020-09-01 22:25:02 -0600

    Added EJS templating

--- server.js
+++ server.js
@@ -14,9 +14,11 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, 'views/index.html'));
+  response.render('index', { list: ['a', 'b'], title: 'Library' });
 });
 
 app.listen(port, () => {
